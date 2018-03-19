fdcc2a77445cdb0b9eb9e46cccc3a58a58bc1d07
Gautam Padiyar <gautam.padiyar@gmail.com>
2018-03-19 19:59:51 +0530

    Navigation view

---
+++ app/src/main/java/org/app/hackio/rajasthanhackathonprototype/MainActivity.java
@@ -0,0 +1,108 @@
+package org.app.hackio.rajasthanhackathonprototype;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    //This is our viewPager
+    private ViewPager viewPager;
+    HomeFragment homeFragment;
+    MarketplaceFragment marketplaceFragment;
+    SuggestionsFragment suggestionsFragment;
+    MenuItem prevMenuItem;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initializing viewPager
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.navigation_marketplace:
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.navigation_suggestions:
+                        viewPager.setCurrentItem(2);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                }
+                else
+                {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: "+position);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+       /*  //Disable ViewPager Swipe
+       viewPager.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                return true;
+            }
+        });
+        */
+
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager)
+    {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        homeFragment = new HomeFragment();
+        marketplaceFragment = new MarketplaceFragment();
+        suggestionsFragment = new SuggestionsFragment();
+        adapter.addFragment(homeFragment);
+        adapter.addFragment(marketplaceFragment);
+        adapter.addFragment(suggestionsFragment);
+
+        viewPager.setAdapter(adapter);
+    }
+
+
+
+}
